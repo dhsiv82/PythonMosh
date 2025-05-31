5053a136bfb058106f20b935af94bee091213548
David Sadler IV <dhsiv82@gmail.com>
2025-05-30 23:35:53 -0400

    Learning With Mosh

---
+++ HelloWorld/strings.py
@@ -0,0 +1,15 @@
+'''
+Learning Strings With Mosh
+'''
+
+course = "Python Programming"
+
+print(len(course))
+
+print(course[0])  # 0 is first
+print(course[-1])  # takes back to first char at end of string
+
+print(course[0:3])  # print string first second and third
+
+print(course[:3])  # everything up to 3rd char
+print(course[:])  # prints every character
